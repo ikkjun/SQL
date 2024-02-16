d3c12cf0296faa3579839e9e34cc0c5b74cd6c77
ikkjun <89628722+ikkjun@users.noreply.github.com>
2024-02-16 15:40:44 +0900

    Update 예제모음.sql

---
+++ JungSuk/예제모음.sql
@@ -0,0 +1,19 @@
+-- 예제 1
+-- 테이블을 생성하시오.
+/*  컬럼명        data type   길이  default
+    id          NUMBER      5
+    name        char        25
+    salary      number      7,2
+    title       char        25      사원
+    in_date     date                sysdate
+    dept_name   char        25
+*/
+
+-- 예제 2
+-- 예제 1에서 생성한 테이블에 데이터를 입력하시오.
+
+-- 예제 3
+-- 예제 2에서 입력한 데이터를 수정하시오.
+
+-- 예제 4
+-- 예제 1에서 생성한 테이블의 모든 데이터를 삭제하시오.
