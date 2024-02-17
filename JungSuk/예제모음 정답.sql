6c8ca809f2a41e852146670c545611f75b25de56
ikkjun <89628722+ikkjun@users.noreply.github.com>
2024-02-17 11:10:33 +0900

    Update 예제모음 정답.sql

--- JungSuk/예제모음 정답.sql
+++ JungSuk/예제모음 정답.sql
@@ -287,6 +287,44 @@ where salary < (select avg(salary)
 ;
 
 -- 48. 본인의 급여가 각 부서별 평균 급여 중 어느 한 부서의 평균 급여보다 적은 급여를 받는 직원에 대해 이름, 급여, 부서번호를 출력하시오.
+select name, salary, dept_id
+from s_emp
+where salary < any(select avg(salary)
+                    from s_emp
+                    group by dept_id)
+;
+
+-- 49. 본인이 다른 사람의 관리자(manager_id)로 되어 있는 직원의 사번, 이름, 직책, 부서번호를 나타내시오.
+select id, name, title, dept_id
+from s_emp outer
+where exists (select id
+                    from s_emp
+                    where outer.id = manager_id)
+;
+
+select id, name, title, dept_id
+from s_emp outer
+where exists (select id
+                    from s_emp
+                    where outer.manager_id = id)
+;
+
+-- 50. 직원(s_emp) 테이블에서 이름을 사전순으로 정렬하여 5개의 데이터만 나타내시오.
+-- 강연범, 구원상, 김석우, 김심선, 김정미 
+select id, name, title, dept_id
+from (select rownum rnum, id, name, title, dept_id
+        from s_emp
+        order by name)
+where rnum <= 5
+;
+
+-- 51. index를 이용해서 title이 사원이 아닌 직원의 id, name, title을 출력하시오.
+select id, name, title
+from s_emp e
+where not exists (select 'X'
+                    from s_emp
+                    where e.title = '사원')
+;
 
 select avg(salary)
 from s_emp
